032322d0386bfbc2e5898bbb11349d9b7c5ea845
Jack Muratore <jackmuratore@gmail.com>
2017-11-28 23:00:15 -0500

    Bad RandomForest and decision tree attempts

--- titantic_kaggle/solve.py
+++ titantic_kaggle/solve.py
@@ -1,14 +1,42 @@
 import pandas as pd
-from sklearn import tree
+from sklearn import tree, preprocessing
+from sklearn.ensemble import RandomForestClassifier
 
-frame = pd.read_csv('./train.csv')
+train_frame = pd.read_csv('./train.csv')
+test_frame = pd.read_csv('./test.csv')
+survive = train_frame['Survived']
 
-clf = tree.DecisionTreeClassifier()
+used_fields = ['Sex']
+final_fields = ['PassengerId', 'Survived']
 
 
-survive = frame['Survived']
+def clean_data(data):
+    d = train_frame.loc[:, used_fields]
+    d = d.fillna(0)
+    le = preprocessing.LabelEncoder()
+    le.fit(['male', 'female'])
+    transform_sex = le.transform(d['Sex'])
+    d['Sex'] = transform_sex
+    return d
 
-# First num is rows 2nd is col index
-data = frame.loc[:, ['Pclass', 'Sex', 'Age', 'Fare', 'Embarked']]
 
-clf = clf.fit(data, survive)
+def classify(sample, test, clf):
+    clf.fit(sample, survive)
+    return clf.predict(test)
+
+
+def solve(fn, clf):
+    train = clean_data(train_frame)
+    test = clean_data(test_frame)
+    r = classify(train, test, clf)
+    original_test = pd.read_csv('./test.csv')
+    original_test['Survived'] = pd.Series(r)
+    original_test = original_test.loc[:, final_fields]
+    original_test.to_csv('{}.csv'.format(fn), index=False)
+
+
+solve('forest', RandomForestClassifier(verbose=True))
+solve('tree', tree.DecisionTreeClassifier())
+
+
+
